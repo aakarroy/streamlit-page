5dd4b854611cce6bdd596575821c2273cd04dcf9
Aakar Roy <aakarroy17@gmail.com>
2026-05-24 02:25:43 +0530

    Updated Page

--- My-projects.py
+++ My-projects.py
@@ -1,45 +1,65 @@
 import streamlit as st
 
-st.title("My Projects: ",text_alignment="justify",)
+st.title("My Projects: ", text_alignment="justify")
 st.space("medium")
-with st.container(width="stretch",horizontal_alignment="center",gap="large"):
-    col1,col2 = st.columns([1,1],vertical_alignment="center",gap="large")
+
+with st.container(width="stretch", horizontal_alignment="center", gap="large"):
+    col1, col2 = st.columns([1,1], vertical_alignment="center", gap="large")
+    
     with col1:
         st.latex(r'''
         \min_{\theta} \left[ \frac{1}{n} \sum_{i=1}^{n} L(f(x_i; \theta), y_i) + \lambda R(\theta) \right]
         ''')
         st.space("stretch")
-        _,cent,_ = st.columns([0.15,1,0.15])
+        _, cent, _ = st.columns([0.1, 1, 0.1])
         with cent:
-            st.link_button("Machine Learning Algorithms","https://github.com/aakarroy/Machine-Learning-Algorithms",)
+            st.link_button("Machine Learning Algorithms", "https://github.com/aakarroy/Machine-Learning-Algorithms", use_container_width=True)
+            
     with col2:
         st.markdown("""
-        <div style="text-align: left; font-family: 'Arial Black', Gadget, sans-serif;padding-left:40px">
+        <div style="text-align: center; font-family: 'Arial Black', Gadget, sans-serif;">
             <span style="color: #3776ab; font-size: 60px; font-weight: 900; letter-spacing: -5px;">PY</span><span style="color: #ffd43b; font-size: 60px; font-weight: 900; letter-spacing: -5px;">THON</span>
         </div>
         """, unsafe_allow_html=True)
-        _,cent,_ = st.columns([0.61,1,0.61])
+        _, cent, _ = st.columns([0.2, 1, 0.2])
         with cent:
-            st.link_button("Python Projects","https://github.com/aakarroy/Nexus-Of-Knowledge-Library-Management-System")
+            st.link_button("Python Projects", "https://github.com/aakarroy/Nexus-Of-Knowledge-Library-Management-System", use_container_width=True)
+            
     st.divider()
-    col3,col4 = st.columns([1,1],vertical_alignment="center",gap="large")
+    
+    col3, col4 = st.columns([1,1], vertical_alignment="center", gap="large")
     with col3:
         st.latex(r'''
         \mathbf{a}^{(l)} = \sigma\left(\mathbf{W}^{(l)}\mathbf{a}^{(l-1)} + \mathbf{b}^{(l)}\right)
         ''')
-        _,cent,_ = st.columns([0.15,1,0.15])
+        _, cent, _ = st.columns([0.1, 1, 0.1])
         with cent:
-            st.link_button("Deep Learning Models","https://github.com/aakarroy/BCI-CNN-LSTM")
+            st.link_button("Deep Learning Models", "https://github.com/aakarroy/BCI-CNN-LSTM", use_container_width=True)
+            
     with col4:
         st.markdown("""
     <div style="
-        text-align: left; 
-        font-family: 'Arial Black', Gadget, sans-serif;
-        padding-left:45px;">
+        text-align: center; 
+        font-family: 'Arial Black', Gadget, sans-serif;">
         <span style="color: #086a35; font-size: 60px; font-weight: 900; letter-spacing: -3px;">Sahi</span><span style="color: #b5614b; font-size: 60px; font-weight: 900; letter-spacing: -3px;">bin</span>
     </div>
     """, unsafe_allow_html=True)
-        _,cent,_ = st.columns([1,1,1])
+        _, cent, _ = st.columns([0.2, 1, 0.2])
         with cent:
-            st.link_button("SAHI-BIN","https://github.com/aakarroy/SahiBin-AI")
-
+            st.link_button("SAHI-BIN", "https://github.com/aakarroy/SahiBin-AI", use_container_width=True)
+            
+    st.divider()
+    
+    col5, col6 = st.columns([1,1], vertical_alignment="center", gap="large")
+    with col5:
+        st.markdown("""
+        <div style="
+            text-align: center; 
+            font-family: 'Arial Black', Gadget, sans-serif;
+            white-space: nowrap;">
+            <span style="color: #00b4d8; font-size: 60px; font-weight: 900; letter-spacing: -3px;">Tooth</span><span style="color: #ff70a6; font-size: 60px; font-weight: 900; letter-spacing: -3px;">Fairy</span>
+        </div>
+        """, unsafe_allow_html=True)
+        _, cent, _ = st.columns([0.2, 1, 0.2])
+        with cent:
+            st.link_button("Tooth Fairy", "https://github.com/aakarroy/ToothFairy2", use_container_width=True)
